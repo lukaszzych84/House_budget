1dd6768a27921095028629421197f6f297a987bd
lukaszzych84 <lukasz251984@gmail.com>
2023-03-15 19:36:22 +0100

    Add admin, first view

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ExpenseCategory, IncomeCategory, Income, Expense
 
-# Register your models here.
+admin.site.register(ExpenseCategory)
+admin.site.register(IncomeCategory)
+admin.site.register(Income)
+admin.site.register(Expense)
+
+
